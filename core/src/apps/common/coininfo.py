7e6b021179ba80047301fad0a010445bf36470fb
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2020-11-09 02:42:47 -0700

    stage Sumcoin w/TODO

--- core/src/apps/common/coininfo.py
+++ core/src/apps/common/coininfo.py
@@ -1494,6 +1494,32 @@ def by_name(name: str) -> CoinInfo:
                 overwintered=False,
                 confidential_assets=None,
             )
+        elif name == "Sumcoin":
+            return CoinInfo(
+                coin_name=name,
+                coin_shortcut="SUM",
+                decimals=8,
+                address_type=63,
+                address_type_p2sh=200,
+                maxfee_kb=1000000,
+                signed_message_header="Sumcoin Signed Message:\n",
+                xpub_magic=0x0488b41c, 
+                xpub_magic_segwit_p2sh=0x01b26ef6,TODO
+                xpub_magic_segwit_native=0x04b24746,TODO
+                bech32_prefix="sum",
+                cashaddr_prefix=None,
+                slip44=2,
+                segwit=True,
+                fork_id=None,
+                force_bip143=False,
+                decred=False,
+                negative_fee=False,
+                curve_name='secp256k1',
+                extra_data=False,
+                timestamp=False,
+                overwintered=False,
+                confidential_assets=None,
+            )
         elif name == "Syscoin":
             return CoinInfo(
                 coin_name=name,
